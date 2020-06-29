32fb4819772d7fd1fe8bd51835a82ab2bec69264
asold <asold@192.168.56.1>
2020-06-29 16:38:47 +0300

    First Example

--- SimpleTextFileExample/src/main/ReadFromTextFileExample.java
+++ SimpleTextFileExample/src/main/ReadFromTextFileExample.java
@@ -1,9 +1,28 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadFromTextFileExample {
 
 	public static void main(String[] args) {
-		
+		//Lucrul cu sistem de fisiere!!! File(clasa)
+		File file = new File("data/person.txt");
+		//vei scana din fisierul indicat
+		try {
+			Scanner in = new Scanner(file);
+			String fname, lname;
+			int age;
+			
+			fname = in.next();
+			lname = in.next();
+			age = in.nextInt();
+			
+			System.out.println(fname + " " + lname + " " + age);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
